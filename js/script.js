aab960cebfc13555c9b16f61b3e9e37ad14a1f05
yeffasol <yeffasol@mail.ru>
2016-07-18 14:43:42 +0300

    Update 2016-07-18T11:43:36.885Z

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-"use strict";!function(){for(var check=document.querySelectorAll(".courses__check"),complete=document.querySelector(".courses__complete"),wrapper=document.querySelector(".courses__wrapper"),title=document.querySelector(".courses__title"),text=document.querySelector(".courses__title ~ p"),i=0;i<check.length;i++)check[i].addEventListener("click",function(e){e.target.parentNode.parentNode.classList.add("courses__item--invisible"),setTimeout(function(){e.target.parentNode.parentNode.nextElementSibling&&e.target.parentNode.parentNode.nextElementSibling.classList.add("courses__item--margin-right"),e.target.parentNode.parentNode.remove(),null==wrapper.firstElementChild&&(complete.classList.add("courses__complete--visible"),title.remove(),text.remove())},1e3)})}();
+'use strict';
+(function () {
+  var check = document.querySelectorAll('.courses__check');
+  var complete = document.querySelector('.courses__complete');
+  var wrapper = document.querySelector('.courses__wrapper');
+  var title = document.querySelector('.courses__title');
+  var text = document.querySelector('.courses__title ~ p');
+
+  function invisible(e) {
+    e.target.parentNode.parentNode.classList.add('invisible');
+  }
+  for (var i = 0; i < check.length; i++) {
+    check[i].addEventListener('click', function (e) {
+      e.target.parentNode.parentNode.classList.add('courses__item--invisible');
+      setTimeout(function () {
+        if (e.target.parentNode.parentNode.nextElementSibling) {
+          e.target.parentNode.parentNode.nextElementSibling.classList.add('courses__item--margin-right');
+        }
+        e.target.parentNode.parentNode.remove(); //.classList.add('courses__item--invisible')
+        if (wrapper.firstElementChild == null) {
+          complete.classList.add('courses__complete--visible');
+          title.remove();
+          text.remove();
+        }
+      }, 1000);
+    });
+  }
+})();
